dda2f8975f0e723d63613cc2bba9a1527f110883
James Mathai <jammathai@gmail.com>
2024-12-24 00:17:57 -0500

    Update cnphylogeny.h

--- cnphylogeny.h
+++ cnphylogeny.h
@@ -1,7 +1,9 @@
 #ifndef CNPHYLOGENY_H
 #define CNPHYLOGENY_H
 
+
 #include <stdlib.h>
+
 
 /**
  * @brief A copy number
@@ -9,52 +11,80 @@
 typedef unsigned char copy_num;
 
 /**
- * @brief A square, right stochastic matrix
- */
-struct prob_matrix {
-    copy_num order; /** The size of the matrix */
-    double probs[]; /** The elements of the matrix */
-};
-
-/**
- * @brief Create a probability matrix
- *
- * Note that the new matrix stores a copy of `probs`, not a reference to it.
- *
- * @param order The size of the matrix
- * @param probs The elements of the matrix, flattened into an array (often, a
- *              compound literal)
- * @return A pointer to the new matrix
- */
-struct prob_matrix *prob_matrix_new(copy_num order, double *probs);
-
-
-/**
  * @brief A binary tree node that stores a copy number profile (CNP)
  * 
  * The fields `left` and `right` should be `NULL` for leaf nodes. For nodes with
- * only one child, `right` should be `NULL`. In other words, if `left` is
- * `NULL`, the node should have no children.
+ * only one child, `right` should be `NULL`. In other words, if a node has no
+ * left child, it shouldn't have a right child either.
  */
 struct cnp_node {
     struct cnp_node *left; /** The left child */
     struct cnp_node *right; /** The right child */
-    size_t len; /** The length of the CNP */
     copy_num bins[]; /** The bins of the CNP */
 };
+
+
+/**
+ * @brief The length of a CNP
+ *
+ * This variable must be defined before calling `cnp_node_new()` or
+ * `phylogeny_optimize()`.
+ */
+extern size_t cnp_len;
+
+/**
+ * @brief The maximum possible copy number
+ *
+ * This variable must be defined before calling `cnp_node_new()` or
+ * `phylogeny_optimize()`.
+ */
+extern copy_num max_copy_num;
+
+/**
+ * @brief A matrix that stores the natural log probabilities of every possible
+ *        configuration of two neighboring bins
+ *
+ * `neighbor_probs[i][j]` gives the log probability of observing a bin with copy
+ * number `i` followed by a bin with copy number `j` (in the same CNP). This
+ * variable must be defined before calling `phylogeny_optimize()`.
+ */
+extern double **neighbor_probs;
+
+/**
+ * @brief A matrix that stores the natural log probabilities of every possible
+ *        configuration of a parent and child bin
+ *
+ * `neighbor_probs[i][j]` gives the log probability of observing a bin with copy
+ * number `i` in a parent CNP and a bin with copy number `j` in the
+ * corresponding position of its child. This variable must be defined before
+ * calling `phylogeny_optimize()`.
+ */
+extern double **mutation_probs;
+
+
+/**
+ * @brief Allocate and assign a probability matrix
+ *
+ * The new matrix will have order `max_copy_num + 1` and will store a copy of
+ * `probs`, not a reference to it.
+ *
+ * @param probs A pointer to the elements of the matrix, flattened into an array
+ *              (often, a compound literal)
+ * @return A pointer to the new matrix
+ */
+double **prob_matrix_new(double *probs);
+
 
 /**
  * @brief Create a new CNP node
  *
  * This function assigns each bin a copy number of 0.
  *
- * @param len The length of the CNP
  * @param left The left child
  * @param right The right child
  * @return A pointer to the new node
  */
 struct cnp_node *cnp_node_new(
-    size_t len,
     struct cnp_node *left,
     struct cnp_node *right
 );
@@ -71,18 +101,12 @@ void cnp_node_free(struct cnp_node *node);
  * @brief Use Gibbs sampling to optimize a phylogeny
  * 
  * @param root A pointer to the root of the phylogeny
- * @param neighbor_probs A probability matrix that defines the likelihood of
- *                       each possible configuration of two neighboring bins
- * @param mutation_probs A probability matrix that defines the likelihood of
- *                       each possible configuration of a parent and child bins
  * @param burn_in The number of samples to discard
  * @param sample_rate The rate at which to record samples
  * @param sample_count The number of samples to record
  */
 void phylogeny_optimize(
     struct cnp_node *root,
-    struct prob_matrix *neighbor_probs,
-    struct prob_matrix *mutation_probs,
     int burn_in,
     int sample_rate,
     int sample_count
