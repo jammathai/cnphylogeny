0a4bfd57b4906a648939d006dd8a1490136fdb51
James Mathai <jammathai@gmail.com>
2025-02-11 14:59:47 -0500

    Update tests

--- tests/cnp_node_new_free.c
+++ tests/cnp_node_new_free.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 
 
-int cnp_len = 1000;
+int cnp_len = 10;
 copy_num max_copy_num;
 double **neighbor_probs;
 double **mutation_probs;
@@ -12,17 +12,30 @@ double **mutation_probs;
 int main()
 {
     struct cnp_node *node = cnp_node_new(
+        0,
         NULL,
         cnp_node_new(
-            NULL,
-            cnp_node_new(NULL, NULL, NULL),
-            cnp_node_new(NULL, NULL, NULL)
+            1,
+            (copy_num []) { 2, 2, 3, 3, 2, 1, 1, 1, 2, 2 },
+            cnp_node_new(2, NULL, NULL, NULL),
+            cnp_node_new(3, NULL, NULL, NULL)
         ),
         NULL
     );
 
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < cnp_len; i++)
         assert(node->bins[i] == 0);
+
+    assert(node->left->bins[0] == 2);
+    assert(node->left->bins[1] == 2);
+    assert(node->left->bins[2] == 3);
+    assert(node->left->bins[3] == 3);
+    assert(node->left->bins[4] == 2);
+    assert(node->left->bins[5] == 1);
+    assert(node->left->bins[6] == 1);
+    assert(node->left->bins[7] == 1);
+    assert(node->left->bins[8] == 2);
+    assert(node->left->bins[9] == 2);
 
     assert(node->left);
     assert(node->left->left);
